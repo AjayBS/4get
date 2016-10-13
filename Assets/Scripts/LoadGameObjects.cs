ce027f2b20f22e09207784821cb787e67af9a90a
AjayBS <bsajay.22@gmail.com>
2016-10-13 00:24:17 -0600

    Set project
    Set the project

---
+++ Assets/Scripts/LoadGameObjects.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using UnityEditor;
+
+public class LoadGameObjects : MonoBehaviour {
+    //Sprite tile;
+    // Use this for initialization
+
+
+    public GameObject brick;
+    
+    void Start () {
+        for (int y = 0; y < 1; y++)
+        {
+            for (int x = -2; x < 3; x++)
+            {
+                GameObject tempBrick;
+                tempBrick = Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                tempBrick.name = "Brick" + x;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        //GameObject go = new GameObject();
+        //SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
+
+        //renderer.sprites[] = Resources.Load("Sprites/Player", typeof(Sprite)) as Sprite;
+
+
+
+        //Sprite[] sprites;
+        //sprites = Resources.LoadAll<Sprite>("Tiles-Sprite");
+        //sprites[0].transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+        //SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+        //if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
+        //    spriteRenderer.sprite = sprites[0];
+        //Debug.Log(sprites.Length);
+    }
+}
---
+++ Assets/Scripts/TileButtons.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class TileButtons : MonoBehaviour {
+
+    GameObject empty_tile;
+    public GameObject[] tiles;
+    Vector3 position;
+    // Use this for initialization
+    void Start () {
+        empty_tile = GameObject.FindGameObjectWithTag("Empty_Tiles");
+        if (tiles == null)
+            tiles = GameObject.FindGameObjectsWithTag("Tiles");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Hello");
+        //    if (Input.GetMouseButtonDown(0))        {
+
+        //        RaycastHit2D hit = Physics2D.Raycast(transform.position,-Vector2.up);
+
+        //        if (hit.collider != null && hit.collider.gameObject.tag == "Tiles")
+        //        {
+        //            Debug.Log("This is a tile click");
+        //        }
+        //    }
+        //}
+    }    
+
+    void OnMouseDown()
+    {
+        position=empty_tile.transform.position;
+        Debug.Log("This is a tile click"+ position);
+        transform.position = position;
+    }
+}
